db5a969e47189534e05453fe15d09cd89df4ca49
Jose Dominguez <jdomindev@gmail.com>
2021-12-16 20:01:16 -0500

    worked on getting color coded text blocks to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,95 @@
-var today
+// add rows and columns
+const hoursOfDay = [
+  "9:00AM",
+  "10:00AM",
+  "11:00AM",
+  "12:00AM",
+  "1:00PM",
+  "2:00PM",
+  "3:00PM",
+  "4:00PM",
+  "5:00PM",
+];
+
+const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
 function setTime() {
-  today = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var today = moment().format("MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").text(today);
 }
 
-setTime();
-setInterval(setTime, 1000);
+function createBody() {
+  // Loop to establish HTML content
+  for (var i = 0; i < hoursOfDay.length; i++) {
+    var container = $(".container");
+    container.addClass("my-5");
+    var row = $("<div>");
+    row.addClass("row");
 
-// add rows and columns
-hoursOfDay = ["9:00AM", "10:00AM", "11:00AM", "12:00AM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
+    // TimeHeader (first column)
+    var timeHeader = $("<div>");
+    timeHeader.addClass(
+      "hour d-flex justify-content-center align-items-center col-xl-1 col-lg-1 col-md-2 col-3"
+    );
+    var timeTitle = $("<div>");
+    timeTitle.text(hoursOfDay[i]);
+    timeHeader.append(timeTitle);
 
+    // TimeBlock (second column)
+    var timeBlock = $("<div>");
+    timeBlock.addClass(
+      "time-block px-0 d-flex justify-content-start align-items-center col-xl-10 col-lg-10 col-md-8 col-6"
+    );
+    var textArea = $("<textarea>");
+    textArea.addClass("textArea h-100 w-100");
+    timeBlock.append(textArea);
 
+    // SaveBtn (third column)
+    var saveBtn = $("<button onclick='saveTextArea()'>");
+    saveBtn.addClass(
+      "saveBtn d-flex justify-content-center align-items-center col-xl-1 col-lg-1 col-md-2 col-3"
+    );
+    saveBtnText = $("<div>");
+    saveBtnText.text("Save 💾");
+    saveBtn.append(saveBtnText);
 
-for (var i = 0; i < hoursOfDay.length; i++){
-var container = $(".container");
-var row = $("<div>");
-row.addClass("row");
+    row.append(timeHeader, timeBlock, saveBtn);
+    container.append(row);
+  }
+}
 
-// TimeHeader (first column)
-var timeHeader = $("<div>");
-timeHeader.addClass("hour d-flex justify-content-center align-items-center col-xl-1 col-lg-1 col-md-2 col-3")
-var timeTitle = $("<div>");
-timeTitle.text(hoursOfDay[i])
-timeTitle.addClass("");
-timeHeader.append(timeTitle);
-
-// TimeBlock (second column)
-var timeBlock = $("<div>");
-timeBlock.addClass("time-block col-xl-10 col-lg-10 col-md-8 col-6");
-var textArea = $("<textarea>");
-textArea.addClass("d-flex justify-content-start")
-timeBlock.append(textArea);
-
-// SaveBtn (third column)
-var saveBtn = $("<div>");
-saveBtn.addClass("saveBtn d-flex justify-content-center align-items-center col-xl-1 col-lg-1 col-md-2 col-3");
-saveBtnText = $("<div>");
-saveBtnText.text("Save")
-saveBtnText.addClass("");
-saveBtn.append(saveBtnText);
-
-
-row.append(timeHeader, timeBlock, saveBtn)
-container.append(row)
+// function to add classes of past present and future based on current hour
+function colorTimeBlocks() {
+  var todayHour = moment().format("H");
+  var timeBlocks = $(".time-block");
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] === parseInt(todayHour)) {
+      $(timeBlocks[i]).addClass("present");
+    } else if (hours[i] < parseInt(todayHour)) {
+      $(timeBlocks[i]).addClass("future");
+    } else if (hours[i] > parseInt(todayHour)) {
+      $(timeBlocks[i]).addClass("past");
+    }
+  }
 }
 
 
-
-
-
-
-
-
-
-
-// function to add classes of past present and future based on time (moment)
-// var timeBlockTime = moment("2021-12-15T09:00:00").format("hh:mm a");
-// console.log(timeBlockTime);
-// var timeBlock = $("#timeBlock");
-// $("#timeBlockTime").text(timeBlockTime);
-
-// function colorTimeBlock() {
-// //   if (timeBlockTime.isSame(today, "hour")) {
-// //     timeBlock.addClass("present");
-// //   } else if (timeBlockTime.isBefore(today)) {
-// //     timeBlock.addClass("future");
-// //   } 
-//   if (moment("9:00","hh:mm").isAfter(today)) {
-//     console.log("past");
-//     timeBlock.addClass("past");
-//   }
+// function saveTextArea() {
+//   var textAreaEntries = [];
+//   var textArea = $("textarea");
+// for (let i = 0; i < hours.length; i++) {
+//   textAreaEntries.push($(textArea[i]).val().trim());
+//   console.log(textAreaEntries);
 // }
+//   localStorage.setItem("text", JSON.stringify(textAreaEntries));
+//   var storedText = JSON.parse(localStorage.getItem(textAreaEntries));
+// }
+
+function onLoad() {
+  setTime();
+  setInterval(setTime, 1000);
+  createBody();
+  colorTimeBlocks();
+}
 
 
